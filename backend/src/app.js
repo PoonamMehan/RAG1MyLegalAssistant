f9b054ab71dd7b884b6ad1fd1692efbe4b06566a
PoonamMehan <poonammehan655@gmail.com>
2025-02-13 20:12:07 +0530

    CORS snd Proy with no full path

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,10 +14,10 @@ const app = express()
 const val = process.env.CORS_ORIGIN
 console.log(val)
 
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true
-// }))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
